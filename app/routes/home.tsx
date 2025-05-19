ee64473a0a7b63e1053728e2f96391bc3c12ccea
AlphaZer00 <batudasdemir@gmail.com>
2025-05-19 12:48:56 -0400

    Change page title

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,17 +2,20 @@ import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Unbound Shop" },
+    { name: "description", content: "Welcome to Unbound, an ecommerce store for clothing, accessories, and more!" },
   ];
 }
 
 export default function Home() {
     return (
-        <main className="flex items-center justify-center pt-16 pb-4"> 
-            <div className="flex flex-col items-center max">
-                <h1 className="text-6xl font-bold pb-4">Home Page</h1>
+        <main className="flex flex-col items-center md:flex-row justify-center pt-16 pb-4"> 
+            <div className="flex flex-col items-center max pb-4">
+                <h1 className="text-6xl font-bold pb-4">Unbound</h1>
                 <p className="max-w-[60ch] w-full px-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eros magna, imperdiet sit amet nulla tempus, luctus placerat lectus. Mauris venenatis consequat aliquet. Donec pretium tincidunt sapien, eu imperdiet lacus eleifend in. Nunc vulputate tortor sit amet turpis pharetra porttitor. Suspendisse blandit vitae ipsum porta tincidunt. Sed nec massa sem. Donec consectetur dolor quis faucibus condimentum. Cras efficitur nibh sit amet posuere porttitor. Donec eget odio pretium, imperdiet turpis a, mollis metus.</p>
+            </div>
+            <div>
+                <img className="w-full max-w-lg h-auto" src="public/images/clothes.jpg" alt="Clothes" />
             </div>
         </main>
     )
