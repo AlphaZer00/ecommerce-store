7f81c200d1ca22a424d4c3812dfc2420723245dc
AlphaZer00 <batudasdemir@gmail.com>
2025-05-19 13:37:02 -0400

    Add animation to home page text

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,5 @@
 import type { Route } from "./+types/home";
+import './animations.css';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,12 +11,15 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
     return (
         <main className="flex flex-col items-center md:flex-row justify-center pt-16 pb-4"> 
-            <div className="flex flex-col items-center max pb-4">
+            <div className="flex flex-col items-center pb-4 fade-in-bottom">
                 <h1 className="text-6xl font-bold pb-4">Unbound</h1>
-                <p className="max-w-[60ch] w-full px-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eros magna, imperdiet sit amet nulla tempus, luctus placerat lectus. Mauris venenatis consequat aliquet. Donec pretium tincidunt sapien, eu imperdiet lacus eleifend in. Nunc vulputate tortor sit amet turpis pharetra porttitor. Suspendisse blandit vitae ipsum porta tincidunt. Sed nec massa sem. Donec consectetur dolor quis faucibus condimentum. Cras efficitur nibh sit amet posuere porttitor. Donec eget odio pretium, imperdiet turpis a, mollis metus.</p>
+                <div className="max-w-[60ch] w-full pb-4">
+                    <p className="pb-4">Step into a curated world of modern fashion where individuality meets versatility. Whether you’re building a timeless wardrobe or chasing the latest trend, our collection is here to   reflect your unique style.</p>
+                    <p>From bold streetwear and tailored essentials to tech accessories and curated extras — we’ve got the look, and a little more.</p>
+                </div>
             </div>
             <div>
-                <img className="w-full max-w-lg h-auto" src="public/images/clothes.jpg" alt="Clothes" />
+                <img className="w-full max-w-lg h-auto" src="/images/clothes.jpg" alt="Clothes" />
             </div>
         </main>
     )
