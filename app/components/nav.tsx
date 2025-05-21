d95b306c0e8ee0dd78deba1e379556d32efca61e
AlphaZer00 <batudasdemir@gmail.com>
2025-05-21 13:21:58 -0400

    Add routing for shop page

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router"
+
 export default function Nav() {
     return (
         <div className="w-full flex justify-center gap-10 p-5  dark:text-white">
             <button className="hover:scale-110 text-black h-full dark:text-white">Home</button>
-            <button className="hover:scale-110 text-black dark:text-white">Shop</button>
+            <Link to="/shop">
+                <button className="hover:scale-110 text-black dark:text-white">Shop</button>
+            </Link>
         </div>
     )
 }
