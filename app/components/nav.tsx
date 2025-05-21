3d72db953d7280c6b5d36c7f2528eaf388c7ed15
AlphaZer00 <batudasdemir@gmail.com>
2025-05-21 13:27:35 -0400

    Add Link to home page

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -3,7 +3,9 @@ import { Link } from "react-router"
 export default function Nav() {
     return (
         <div className="w-full flex justify-center gap-10 p-5  dark:text-white">
-            <button className="hover:scale-110 text-black h-full dark:text-white">Home</button>
+            <Link to="/">
+                <button className="hover:scale-110 text-black h-full dark:text-white">Home</button>
+            </Link>
             <Link to="/shop">
                 <button className="hover:scale-110 text-black dark:text-white">Shop</button>
             </Link>
