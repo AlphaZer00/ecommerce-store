39bc5d2b404451e2a1e61a74f7a65f5d7f95fce9
AlphaZer00 <batudasdemir@gmail.com>
2025-05-21 12:11:21 -0400

    Add styling

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -11,17 +11,20 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
     return (
-        <main className="flex flex-col items-center md:flex-row justify-center pt-16 pb-4"> 
-            <div className="flex flex-col items-center pb-4 fade-in-bottom">
-                <h1 className="text-6xl font-bold pb-4">Unbound</h1>
-                <div className="max-w-[60ch] w-full pb-4">
-                    <p className="pb-4">Step into a curated world of modern fashion where individuality meets versatility. Whether you’re building a timeless wardrobe or chasing the latest trend, our collection is here to   reflect your unique style.</p>
-                    <p>From bold streetwear and tailored essentials to tech accessories and curated extras — we’ve got the look, and a little more.</p>
+        <>
+            <Nav></Nav>
+            <main className="flex flex-col items-center lg:flex-row justify-center pt-4 sm:pt-0 pb-4 fade-in-bottom ml-10 mr-10"> 
+                <div className="flex flex-col items-center pb-4 ">
+                    <h1 className="text-6xl font-bold pb-4">Unbound</h1>
+                    <div className="max-w-[60ch] w-full pb-4">
+                        <p className="pb-4">Step into a curated world of modern fashion where individuality meets versatility. Whether you’re building a timeless wardrobe or chasing the latest trend, our collection is here to   reflect your unique style.</p>
+                        <p>From bold streetwear and tailored essentials to tech accessories and curated extras — we’ve got the look, and a little more.</p>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <img className="w-full max-w-lg h-auto" src="/images/clothes.jpg" alt="Clothes" />
-            </div>
-        </main>
+                <div>
+                    <img className="w-full max-w-lg h-auto" src="/images/clothes.jpg" alt="Clothes" />
+                </div>
+            </main>
+        </>
     )
 }
