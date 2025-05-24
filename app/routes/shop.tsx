af3a713b027ac7007a331d1f4dfe98f4c67af705
AlphaZer00 <batudasdemir@gmail.com>
2025-05-24 09:26:13 -0400

    Add grid layout

--- app/routes/shop.tsx
+++ app/routes/shop.tsx
@@ -11,17 +11,17 @@ export default function Shop() {
     }, []);
 
     return (
-        <div>
+        <div className="flex flex-col justify-center items-center">
             <Nav></Nav>
-            <h1>Shop</h1>
-            <ul>
+            <h1 className="text-4xl font-bold pb-4">Shop</h1>
+            <ul className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4">
                 {products.map((product) => (
                     <li key={product.id} className="mb-2">
                         <p>{product.title}</p>
                         <img
                             src={product.image}
                             alt={product.title}
-                            className="w-20"
+                            className="w-30"
                         />
                     </li>
                 ))}
