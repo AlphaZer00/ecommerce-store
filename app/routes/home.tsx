ca281172975e93a6e6b1af13c1f1dde5410f566d
AlphaZer00 <batudasdemir@gmail.com>
2025-05-24 09:25:54 -0400

    Change header size

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             <Nav></Nav>
             <main className="flex flex-col items-center lg:flex-row justify-center pt-4 sm:pt-0 pb-4 fade-in-bottom ml-10 mr-10"> 
                 <div className="flex flex-col items-center pb-4 ">
-                    <h1 className="text-6xl font-bold pb-4">Unbound</h1>
+                    <h1 className="text-4xl font-bold pb-4">Unbound</h1>
                     <div className="max-w-[60ch] w-full pb-4">
                         <p className="pb-4">Step into a curated world of modern fashion where individuality meets versatility. Whether you’re building a timeless wardrobe or chasing the latest trend, our collection is here to   reflect your unique style.</p>
                         <p>From bold streetwear and tailored essentials to tech accessories and curated extras — we’ve got the look, and a little more.</p>
