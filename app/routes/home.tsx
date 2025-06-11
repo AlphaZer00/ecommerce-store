33b111a8c5179c316e95fbab78192ff371c3a45e
AlphaZer00 <batudasdemir@gmail.com>
2025-06-11 11:45:17 -0400

    Add footer to home & shop pages

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
 import type { Route } from "./+types/home";
 import "./animations.css";
 import Nav from "../components/Nav";
+import Footer from "~/components/Footer";
 
 export function meta({}: Route.MetaArgs) {
     return [
@@ -15,9 +16,9 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
     return (
-        <>
+        <div className="flex flex-col justify-center min-h-screen">
             <Nav></Nav>
-            <main className="flex flex-col items-center lg:flex-row justify-center pt-4 sm:pt-0 pb-4 fade-in-bottom ml-10 mr-10">
+            <main className="flex flex-col grow-1 shrink-0 basis-auto items-center lg:flex-row justify-center pt-4 sm:pt-0 pb-4 fade-in-bottom px-10 text-black bg-[#D8CCC4]">
                 <div className="flex flex-col items-center pb-4 ">
                     <h1 className="text-4xl font-bold pb-4">Unbound</h1>
                     <div className="max-w-[60ch] w-full pb-4">
@@ -43,6 +44,7 @@ export default function Home() {
                     />
                 </div>
             </main>
-        </>
+            <Footer></Footer>
+        </div>
     );
 }
--- app/routes/shop.tsx
+++ app/routes/shop.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { fetchProducts } from "../api/fakeStoreApi";
 import type { Product } from "../types/product";
 import ProductCard from "~/components/ProductCard";
+import Footer from "~/components/Footer";
 
 export default function Shop() {
     const [products, setProducts] = useState<Product[]>([]);
@@ -12,19 +13,24 @@ export default function Shop() {
     }, []);
 
     return (
-        <div className="flex flex-col justify-center items-center pr-5 pl-5">
+        <div className="flex flex-col justify-center">
             <Nav></Nav>
-            <h1 className="text-4xl font-bold pb-4">Shop</h1>
-            <ul className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4">
-                {products.map((product) => (
-                    <li key={product.id} className="mb-2">
-                        <ProductCard
-                            product={product}
-                            onAddToCart={(p) => console.log("Added to cart", p)}
-                        ></ProductCard>
-                    </li>
-                ))}
-            </ul>
+            <main className="grow-1 px-5 pb-5 flex flex-col align-middle items-center justify-center">
+                <h1 className="text-4xl font-bold pb-4">Shop</h1>
+                <ul className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4">
+                    {products.map((product) => (
+                        <li key={product.id} className="mb-2">
+                            <ProductCard
+                                product={product}
+                                onAddToCart={(p) =>
+                                    console.log("Added to cart", p)
+                                }
+                            ></ProductCard>
+                        </li>
+                    ))}
+                </ul>
+            </main>
+            <Footer></Footer>
         </div>
     );
 }
