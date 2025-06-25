a24a6692a1fd4e796b57bd009841ce3dbd993ce8
AlphaZer00 <batudasdemir@gmail.com>
2025-06-25 11:20:38 -0400

    Update item styling in cart

--- app/routes/cart.tsx
+++ app/routes/cart.tsx
@@ -26,42 +26,57 @@ export default function Cart() {
     return (
         <>
             <Nav></Nav>
-            <div>
-                <h1>Cart</h1>
-                <ul className="flex flex-col gap-2">
-                    {cartItems.map((item) => (
-                        <li key={item.id}>
-                            <h2>{item.title}</h2>
-                            <img
-                                src={item.image}
-                                alt={item.title}
-                                className="w-40 h-40 object-contain mb-4"
-                            />
-                            <div className="mb-2">
-                                <label className="mr-2">Quantity:</label>
-                                <input
-                                    className="max-w-15"
-                                    type="number"
-                                    min="1"
-                                    value={item.quantity}
-                                    onChange={(e) =>
-                                        updateQuantity(
-                                            item.id,
-                                            parseInt(e.target.value)
-                                        )
-                                    }
-                                />
-                            </div>
-                            <p>${(item.price * item.quantity).toFixed(2)}</p>
-                            <button
-                                className="mt-2 px-3 bg-red-500 text-white rounded hover:bg-red-600"
-                                onClick={() => removeFromCart(item)}
-                            >
-                                Remove
-                            </button>
-                        </li>
-                    ))}
-                </ul>
+            <div className="">
+                <h1 className="text-3xl font-bold">Cart</h1>
+                <div className="flex flex-col">
+                    <ul className="flex flex-col gap-4 border rounded-xl !p-5">
+                        {cartItems.map((item) => (
+                            <li className="flex" key={item.id}>
+                                <div className="image-container bg-white rounded-2xl p-2 mr-2">
+                                    <img
+                                        src={item.image}
+                                        alt={item.title}
+                                        className="w-30 h-30 object-contain"
+                                    />
+                                </div>
+                                <div className="info flex flex-col flex-grow">
+                                    <div className="top-line flex justify-between">
+                                        <h2>{item.title}</h2>
+                                        <button
+                                            className="mt-2 px-3 bg-red-500 text-white rounded hover:bg-red-600"
+                                            onClick={() => removeFromCart(item)}
+                                        >
+                                            Remove
+                                        </button>
+                                    </div>
+                                    <div className="Description text-xs text-gray-400 max-w-xl">
+                                        {item.description}
+                                    </div>
+                                    <div className="flex justify-between">
+                                        <p className="font-medium">
+                                            $
+                                            {(
+                                                item.price * item.quantity
+                                            ).toFixed(2)}
+                                        </p>
+                                        <input
+                                            className="max-w-15"
+                                            type="number"
+                                            min="1"
+                                            value={item.quantity}
+                                            onChange={(e) =>
+                                                updateQuantity(
+                                                    item.id,
+                                                    parseInt(e.target.value)
+                                                )
+                                            }
+                                        />
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
             <Footer></Footer>
         </>
