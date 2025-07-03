d02b8b5d63ccbc0a2fa0797460175cbb5b88c725
AlphaZer00 <batudasdemir@gmail.com>
2025-07-03 11:25:55 -0400

    Add Order Summary section

--- app/routes/cart.tsx
+++ app/routes/cart.tsx
@@ -23,16 +23,25 @@ export default function Cart() {
         );
     };
 
+    const subTotal = cartItems.reduce(
+        (total, item) => total + item.price * item.quantity,
+        0
+    );
+
+    const salesTax = subTotal * 0.08875;
+
+    const totalPrice = subTotal + salesTax + 5;
+
     return (
-        <>
+        <div className="flex flex-col justify-center min-h-screen">
             <Nav></Nav>
-            <div className="m-4">
-                <h1 className="text-3xl font-bold">Cart</h1>
-                <div className="flex flex-col">
-                    <ul className="flex flex-col gap-4 border rounded-xl !p-5">
+            <div className="grow-1 flex flex-col items-center m-4 fade-in-bottom">
+                <h1 className="text-3xl font-bold pb-4">Shopping Cart</h1>
+                <div className="flex flex-col justify-center">
+                    <ul className="flex flex-col gap-4 border rounded-xl !p-5 !mb-5">
                         {cartItems.map((item) => (
                             <li className="flex" key={item.id}>
-                                <div className="image-container bg-white rounded-2xl p-2 mr-2">
+                                <div className="image-container bg-white rounded-2xl p-2 mr-4">
                                     <img
                                         src={item.image}
                                         alt={item.title}
@@ -77,8 +86,30 @@ export default function Cart() {
                         ))}
                     </ul>
                 </div>
+                <div className="order-summary flex flex-col gap-1 max-w-2xs border rounded-xl p-5 min-w-2xs">
+                    <h1 className="font-bold">Order Summary</h1>
+                    <div className="flex justify-between">
+                        <p>Subtotal:</p>
+                        <p className="font-semibold">${subTotal.toFixed(2)}</p>
+                    </div>
+                    <div className="flex justify-between">
+                        <p>Sales Tax:</p>
+                        <p className="font-semibold">${salesTax.toFixed(2)}</p>
+                    </div>
+                    <div className="flex justify-between pb-1">
+                        <p>Delivery Fee:</p>
+                        <p className="font-semibold">$5.00</p>
+                    </div>
+                    <hr className="p-1" />
+                    <div className="flex justify-between">
+                        <p>Total Price:</p>
+                        <p className="font-semibold">
+                            ${totalPrice.toFixed(2)}
+                        </p>
+                    </div>
+                </div>
             </div>
             <Footer></Footer>
-        </>
+        </div>
     );
 }
